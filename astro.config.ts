5dd93c4d4c9fb2d196183a4120df00e9072aec86
0x7C00 <loganqiu1020@outlook.com>
2025-08-26 17:24:26 +0800

    changes a lot

--- astro.config.ts
+++ astro.config.ts
@@ -1,54 +1,16 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
-import remarkAlert from "remark-github-blockquote-alert";
-import { SITE } from "./src/config";
-
-// https://astro.build/config
-export default defineConfig({
-  site: SITE.website,
-  integrations: [sitemap({
-    filter: page => SITE.showArchives || !page.endsWith("/archives"),
-  }), partytown({ config: { forward: ['dataLayer.push', 'gtag'] } })],
-  markdown: {
-    remarkPlugins: [remarkMath, remarkAlert],
-    rehypePlugins: [rehypeKatex],
-    shikiConfig: {
-      // For more themes, visit https://shiki.style/themes
-      themes: { light: "one-light", dark: "night-owl" },
-      wrap: true,
-    },
-  },
-  vite: {
-    plugins: [tailwindcss()],
-    optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
-    },
-  },
-  image: {
-    // Used for all Markdown images; not configurable per-image
-    // Used for all `<Image />` and `<Picture />` components unless overridden with a prop
-    experimentalLayout: "responsive",
-  },
-  experimental: {
-    svg: true,
-    responsiveImages: true,
-    preserveScriptOrder: true,
-  },
-});import { defineConfig, envField } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
-import sitemap from "@astrojs/sitemap";
-import remarkToc from "remark-toc";
-import remarkCollapse from "remark-collapse";
 import {
   transformerNotationDiff,
   transformerNotationHighlight,
   transformerNotationWordHighlight,
 } from "@shikijs/transformers";
 import { transformerFileName } from "./src/utils/transformers/fileName";
+import remarkAlert from "remark-github-blockquote-alert";
 import { SITE } from "./src/config";
 
 // https://astro.build/config
@@ -58,12 +20,14 @@ export default defineConfig({
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
     }),
+    partytown({ config: { forward: ["dataLayer.push", "gtag"] } }),
   ],
   markdown: {
-    remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
+    remarkPlugins: [remarkMath, remarkAlert],
+    rehypePlugins: [rehypeKatex],
     shikiConfig: {
       // For more themes, visit https://shiki.style/themes
-      themes: { light: "min-light", dark: "night-owl" },
+      themes: { light: "one-light", dark: "night-owl" },
       defaultColor: false,
       wrap: false,
       transformers: [
